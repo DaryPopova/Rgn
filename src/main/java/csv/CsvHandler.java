101eb144efbe13ee0606bf5267d34ca27fac8f88
padre <pd2405@yandex.ru>
2019-05-15 18:58:13 +0300

    классы для csv

---
+++ src/main/java/csv/CsvHandler.java
@@ -0,0 +1,13 @@
+package csv;
+
+import java.util.ArrayList;
+
+public class CsvHandler {
+    public ArrayList<Entity> readCsvToListOfEntities(Class typeOfEntity, String path) {
+        return new ArrayList<>();
+    }
+
+    public void writeEntitiesToCsv(ArrayList<Entity> entities) {
+
+    }
+}
